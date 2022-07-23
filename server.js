3568fe466754dfef16dcad9db65986e9cfe8b8f7
ctnohilly <ctnohilly@gmail.com>
2022-07-23 01:01:41 -0400

    7/23 commit all current changes

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'root',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
